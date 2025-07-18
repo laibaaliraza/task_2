d84bc16d2ed9deedd4d5aa07c807ecda30644af3
laibaaliraza <laiba.aliraza2203@gmail.com>
2025-07-19 02:20:21 +0500

    Backup for 20250719

--- search_backup.sh
+++ search_backup.sh
@@ -25,40 +25,37 @@ fi
 
 #make backup directory if directory does not exists
 mkdir -p "$backup_dir"
-cd "$backup_dir" || exit 1
 
 #Use of rsync(-i is used in cronjob.txt)
 if [ "$i_tag" = true ]; then       #incremental backup
   dest_dir="incremental-$date"
-  prev_dir=$(ls -dt incremental-* 2>/dev/null | head -n 1)
-   if [ -n "$prev_dir" ]; then
-     rsync -a --link-dest="../$prev_dir" "$dir/" "$dest_dir/" || exit 2
-   else
-     rsync -a "$dir/" "$dest_dir/" || exit 2
-   fi
- else
-   dest_dir="full-$date"    #do full backup(-i was not used)
-   rsync -a "$dir/" "$dest_dir/" || exit 2
- fi
- 
- cd ..                             #root task_2
- 
- #make report.txt file
- if [ "$report" = true ]; then
-   count=$(find "$backup_dir/$dest_dir" -type f | wc -l)
-   size=$(du -sh "$backup_dir/$dest_dir" | awk '{print$1}')
-   echo "$count files archived, total $size" > report.txt
- fi
- 
- 
+  prev_dir=$(ls -dt "$backup_dir"/incremental-* 2>/dev/null | head -n 1)
+  if [ -n "$prev_dir" ]; then
+    rsync -a --link-dest="$prev_dir" "$dir/" "$backup_dir/$dest_dir/" || exit 2
+  else
+    rsync -a "$dir/" "$backup_dir/$dest_dir/" || exit 2
+  fi
+else
+  dest_dir="full-$date"    #do full backup(-i was not used)
+  rsync -a "$dir/" "$backup_dir/$dest_dir/" || exit 2
+fi
+
+#make report.txt file
+if [ "$report" = true ]; then
+  count=$(find "$backup_dir/$dest_dir" -type f | wc -l)
+  size=$(du -sh "$backup_dir/$dest_dir" | awk '{print$1}')
+  echo "$count files archived, total $size" > report.txt
+fi
+
+git_backup="backup-$date"
+
 if git rev-parse --git-dir > /dev/null 2>&1; then
   git add .
   git commit -m "Backup for $date"
   git tag "$git_backup"
-  git push origin enhanced_backup
+  git push origin enhanced-backup
   git push origin "$git_backup"
 else
   echo "Not a git repository, skipping version control steps."
 fi
-
 
